af37e17812642d9257b8e0ce520719b0a127d4d6
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-19 18:42:10 +0300

    initial commmit

--- script.js
+++ script.js
@@ -1,101 +1,111 @@
-// // // // const shoulDrive = true;
-// // // // const speedLimit = 50;
-// // // // const driver = 'serah';
+// // // // // const shoulDrive = true;
+// // // // // const speedLimit = 50;
+// // // // // const driver = 'serah';
 
 
 
-// // // // if (shoulDrive) {
-// // // //     console.log(`${driver} is driving`);
-// // // // } else {
-// // // //     console.log(`${driver} is not driving`);
+// // // // // if (shoulDrive) {
+// // // // //     console.log(`${driver} is driving`);
+// // // // // } else {
+// // // // //     console.log(`${driver} is not driving`);
+// // // // // }
+// // // // // // Output: Serah is driving
+
+
+
+// // // // // const scoreDelphins = (96 + 108 + 89 ) / 3 ;
+// // // // // const scoreKoalas = (88 + 89 + 110 ) /3 ;
+
+// // // // // if (scoreDelphins > scoreKoalas) {
+// // // // //     console.log('The Delphins are the winners');
+// // // // // } else if (scoreKoalas > scoreDelphins) {
+// // // // //     console.log('The Koalas are the winners');
+// // // // // } else if (scoreKoalas === scoreDelphins) {
+// // // // //     console.log('both are the winners');
+// // // // // }
+
+
+// // // // // // calculating the average score
+// // // // // const totalScore = scoreDelphins + scoreKoalas;
+// // // // // const averageScore = totalScore / 2;
+// // // // // console.log(`The average score is ${averageScore}`);
+
+
+// // // // // const sumDelphins = (96 + 108 + 89);
+// // // // // const sumKoalas = (88 + 110 + 89);
+// // // // // const totalMarks = scoreDelphins + scoreKoalas;
+
+// // // // // console.log(`The average score is ${totalMarks}`);
+
+
+
+// // // // const day = "Tuesday";
+// // // // const rainDay = "true";
+
+// // // // switch (day) {
+// // // //     case "Monday":
+// // // //         console.log("Go to work");
+// // // //         break;
+// // // //     case "Tuesday":
+// // // //         console.log("Go to Playground");
+// // // //         break;
+// // // //     case "Wednesday":
+// // // //         console.log("Go to Gym");
+// // // //         break;
+// // // //     case "Thursday":
+// // // //         console.log("Go to School");
+// // // //         break;
+// // // //     case "Friday":
+// // // //         console.log("Go to Home");
+// // // //         break;
+// // // //     case "Saturday":
+// // // //         console.log("Go to Beach");
+// // // //         break;
+// // // //     case "Sunday":
+// // // //         console.log("Go to Sleep");
+// // // //         break;
+// // // //     default:
+// // // //         console.log("Invalid day");
 // // // // }
-// // // // // Output: Serah is driving
 
-
-
-// // // // const scoreDelphins = (96 + 108 + 89 ) / 3 ;
-// // // // const scoreKoalas = (88 + 89 + 110 ) /3 ;
-
-// // // // if (scoreDelphins > scoreKoalas) {
-// // // //     console.log('The Delphins are the winners');
-// // // // } else if (scoreKoalas > scoreDelphins) {
-// // // //     console.log('The Koalas are the winners');
-// // // // } else if (scoreKoalas === scoreDelphins) {
-// // // //     console.log('both are the winners');
+// // // // if (day === "Monday") {
+// // // //     console.log("Go to work");
+// // // // } else if (day === "Tuesday") {
+// // // //     console.log("Go to Playground");
+// // // // } else if (day === "Wednesday" || day === "Thursday") {
+// // // //     console.log("Go to Gym");
+// // // // } else if (day === "Friday") {
+// // // //     console.log("Go to Home");
+// // // // } else if (day === "Saturday" || day === "Sunday") {
+// // // //     console.log("Go to Beach");
+// // // // } else{
+// // // //     console.log("Invalid day");
 // // // // }
 
 
-// // // // // calculating the average score
-// // // // const totalScore = scoreDelphins + scoreKoalas;
-// // // // const averageScore = totalScore / 2;
-// // // // console.log(`The average score is ${averageScore}`);
+
+// // //   if (23>10){
+// // //     const str = '23 is bigger';
+// // //   }
+
+// // //   const me = 'watty'
+// // //   console.log(`I'm ${2037-1991} years old ${me}`);
 
 
-// // // // const sumDelphins = (96 + 108 + 89);
-// // // // const sumKoalas = (88 + 110 + 89);
-// // // // const totalMarks = scoreDelphins + scoreKoalas;
-
-// // // // console.log(`The average score is ${totalMarks}`);
-
-
-
-// // // const day = "Tuesday";
-// // // const rainDay = "true";
-
-// // // switch (day) {
-// // //     case "Monday":
-// // //         console.log("Go to work");
-// // //         break;
-// // //     case "Tuesday":
-// // //         console.log("Go to Playground");
-// // //         break;
-// // //     case "Wednesday":
-// // //         console.log("Go to Gym");
-// // //         break;
-// // //     case "Thursday":
-// // //         console.log("Go to School");
-// // //         break;
-// // //     case "Friday":
-// // //         console.log("Go to Home");
-// // //         break;
-// // //     case "Saturday":
-// // //         console.log("Go to Beach");
-// // //         break;
-// // //     case "Sunday":
-// // //         console.log("Go to Sleep");
-// // //         break;
-// // //     default:
-// // //         console.log("Invalid day");
-// // // }
-
-// // // if (day === "Monday") {
-// // //     console.log("Go to work");
-// // // } else if (day === "Tuesday") {
-// // //     console.log("Go to Playground");
-// // // } else if (day === "Wednesday" || day === "Thursday") {
-// // //     console.log("Go to Gym");
-// // // } else if (day === "Friday") {
-// // //     console.log("Go to Home");
-// // // } else if (day === "Saturday" || day === "Sunday") {
-// // //     console.log("Go to Beach");
-// // // } else{
-// // //     console.log("Invalid day");
-// // // }
-
-
-
-// //   if (23>10){
-// //     const str = '23 is bigger';
-// //   }
-
-// //   const me = 'watty'
-// //   console.log(`I'm ${2037-1991} years old ${me}`);
+// // const age = '23'
+// // age >= '18' ? console.log (`I like to drink wine`) : console.log (`I like to drink water`);
 
 
 // const age = '23'
-// age >= '18' ? console.log (`I like to drink wine`) : console.log (`I like to drink water`);
+// const canDrink = age >= '18' ? ('wine') : 'water';  
+// console.log(canDrink);
 
 
-const age = '23'
-const canDrink = age >= '18' ? ('wine') : 'water';  
-console.log(canDrink);
+function creator() {
+    console.log('I am a creator');
+}
+
+
+creator();
+creator();
+creator();
