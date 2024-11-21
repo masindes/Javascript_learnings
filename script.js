02cc298c602e5a37128fb2d689c936b388507dc2
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-21 23:04:12 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-// Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+// // Remember, we're gonna use strict mode in all scripts now!
+// 'use strict';
 
 
-document.querySelector('h1').textContent = 'JavaScript and react are amazing!';
+// document.querySelector('h1').textContent = 'JavaScript and react are amazing!';
 
-console.log(document.querySelector('h1').textContent);
+// console.log(document.querySelector('h1').textContent);
 
-// console.log(document.querySelector('.score').textContent);
+// // console.log(document.querySelector('.score').textContent);
 
-// console.log(document.querySelector('.number').textContent);
+// // console.log(document.querySelector('.number').textContent);
 
-document.querySelector('.score').textContent = 13
-document.querySelector('.number').textContent = 17
+// document.querySelector('.score').textContent = 13
+// document.querySelector('.number').textContent = 17
 
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value =10;
+// document.querySelector('.guess').value =23;
+// console.log(document.querySelector('.guess').value);
+
+
+
+
+const numbers = Math.random();
+
+document.querySelector('.check').addEventListener('click', function(e) {
+    const guess =Number(document.querySelector('.guess').value);
+    console.log(guess,typeof guess);
+
+    if(!guess) {
+        
+        document.querySelector('.message').textContent = 'No number provided!'
+        alert('No number provided!');
+        return;
+        
+    }
+
+});
