2de60b92cb2e9165e66813cc22bc488c0a09a9db
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-21 21:38:32 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
 
+document.querySelector('h1').textContent = 'JavaScript and react are amazing!';
 
-const calcAge = birthYear => 2037 - birthYear
+console.log(document.querySelector('h1').textContent);
 
-console.log(1991);
+// console.log(document.querySelector('.score').textContent);
+
+// console.log(document.querySelector('.number').textContent);
+
+document.querySelector('.score').textContent = 13
+document.querySelector('.number').textContent = 17
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value =10;
